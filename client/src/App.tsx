4531f5ee0ae6e85e8f45b1bba0002bac0eb1430a
Frozen55Git <michael.wisis0505@gmail.com>
2025-03-21 14:59:09 +0900

    app.js updated

---
+++ client/src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Navbar from "./components/Navbar/Navbar";
+import {Route, Routes} from "react-router-dom";
+import Register from "./components/Register";
+import LeagueList from "./components/LeagueList";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import LeagueClubs from "./components/LeagueClubs";
+
+function App() {
+
+
+    return (
+        <div>
+            <Navbar/>
+            <Routes>
+                <Route path={'/'} element={<Home/>}/>
+                <Route path={'/login'} element={<Login/>}/>
+                <Route path={'/register'} element={<Register/>}/>
+                <Route path={'/leagues'} element={<LeagueList/>}/>
+                <Route path={'/leagues/:id'} element={<LeagueClubs/>}/>
+            </Routes>
+        </div>
+
+    );
+}
+
+export default App;
